a92afa74643fa7df241e21112d68870a7fb58b27
slavicf <jaroslavfrunt@gmail.com>
2018-10-13 20:52:52 +0300

    Restruct to objects

--- CorridorLight/CorridorLight.ino
+++ CorridorLight/CorridorLight.ino
@@ -5,64 +5,122 @@
 #define SENSOR_1  11
 #define LEVEL_MIN 0
 #define LEVEL_MAX 127
-#define PERIOD 1000 * 1000 / 4 // period of timer in us
+#define PERIOD 1000 / 4 // period of timer in ms
 #define SHINE 10 * 1000  // time of shining in ms
 
 #define PIXEL_COUNT 16
 #define PIXEL_PIN 12  // Digital IO pin connected to the NeoPixels.
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(PIXEL_COUNT, PIXEL_PIN, NEO_GRB + NEO_KHZ800);
 
-long level = 0;
-boolean dir = true;
-boolean sensor;
-byte tic = 0;
-boolean ticBit[8] = {0, 0, 0, 0, 0, 0, 0, 0};
-boolean ticBit1[8] = {0, 0, 0, 0, 0, 0, 0, 0};
-unsigned long timer[4] = {0, 0, 0, 0};
-boolean timer0;
+class DetectEdge {
+    bool neg = false;
+    bool pos = false;
+    bool stateNeg = false;
+    bool statePos = false;
+
+  public:
+    //    Button(byte attachTo) :
+    //      pin(attachTo)
+    //    {
+    //    }
+
+    //    void setup() {
+    //
+    //    }
+
+    void detect(bool state) {
+      neg = state && !stateNeg; // detect negative edge
+      stateNeg = state;         // store old value
+      pos = !state && statePos; // detect positive edge
+      statePos = state;         // store old value
+    }
+
+    bool getNeg() {
+      return neg;
+    }
+
+    bool getPos() {
+      return pos;
+    }
+};
+
+class Input {
+    const byte pin;
+    //    int state;
+    //    unsigned long debounceMs;
+
+  public:
+    Input(byte attachTo) :
+      pin(attachTo)
+    {
+    }
+
+    void setup() {
+      pinMode(pin, INPUT);
+      //      state = HIGH;
+    }
+
+    bool getState() {
+      return digitalRead(pin);
+    }
+};
+
+class NeoPixel {
+    long level = 0;
+    unsigned long storageMs = 0;
+
+  public:
+//    void setup() {
+//
+//    }
+
+  void shine(bool dir) {
+    if (millis() - storageMs >= PERIOD
+    ) {
+      changeLevel(dir);
+      
+      uint32_t color = level + (level << 8) + (level << 16);
+      for (uint16_t i = 0; i < PIXEL_COUNT; i++) {
+        strip.setPixelColor(i, color);
+      }
+      strip.show();
+
+      storageMs = millis();
+    }
+  }
+
+  void changeLevel(bool dir) {
+    if (dir) level++;
+    else level--;
+    if (level < LEVEL_MIN) level = LEVEL_MIN;
+    if (level > LEVEL_MAX) level = LEVEL_MAX;
+  }
+};
+
+DetectEdge sensor();
+Input sensor0(SENSOR_0);
+Input sensor1(SENSOR_1);
 
 void setup() {
-  Timer1.initialize(PERIOD); // initialize timer
-  Timer1.attachInterrupt(timerTic); // attach the service routine here
-  pinMode(SENSOR_0, INPUT);
-  pinMode(SENSOR_1, INPUT);
+  sensor0.setup();
+  sensor1.setup();
   pinMode(LED_BUILTIN, OUTPUT);
   strip.begin();
   strip.show(); // Initialize all pixels to 'off'
   //  Serial.begin(9600);
+  //  Serial.print("Level: ");
 }
 
 void loop() {
-  timeBits(); // setting positive edges
-  sensor = digitalRead(SENSOR_0) || digitalRead(SENSOR_1);
-  if (sensor) {
-    timer[0] = millis() + SHINE;
-  }
-  timer0 = millis() < timer[0];
-  digitalWrite(LED_BUILTIN, sensor);
 
-  if (ticBit[0]) {
-    levels();
-  }
 }
 
-// timer interrupt
-void timerTic() {
-  tic++;
-}
+// WS2812 driver
+void levels() {
 
-// setting positive edges
-void timeBits() {
-  for (int i = 0; i < 8; i++) {
-    if (bitRead(tic, i)) {
-      if (!ticBit1[i]) {
-        ticBit[i] = true;
-        ticBit1[i] = true;
-      } else ticBit[i] = false;
-    } else {
-      ticBit[i] = false;
-      ticBit1[i] = false;
-    }
-  }
+
+
+  //  Serial.println(level);
+
 }
 
